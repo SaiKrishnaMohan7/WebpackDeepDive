1b17292e7e9b92636a36735cccaab82937586ae0
Sai Krishna Mohan <saikrishnamohan@ibm.com>
2020-03-15 13:59:39 -0400

    Add first plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
+const { ProgressPlugin } = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
 // Could use exports but we don't be more specific. Attributing to this link  https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 // This object is used ot override webpack 4's default behaviours
 module.exports = ({ mode }) => ({
   mode,
   output: {
     filename: 'bundle.js'
-  }
+  },
+  plugins: [
+    new HTMLWebpackPlugin(), // injects all output assets into the HTML file (<script />)
+    new ProgressPlugin(),
+  ],
 });
 
 /**
