bd977af14a36c45f7cc46f47a0aa72e67ce82cdc
Sai Krishna Mohan <saikrishnamohan@ibm.com>
2020-03-15 13:46:21 -0400

    pass in vars into webpack config from cli

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 // Could use exports but we don't be more specific. Attributing to this link  https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 // This object is used ot override webpack 4's default behaviours
-module.exports = {
-  mode: 'none' // bundle it but don't wrap it in eval etc and go ahead with default behaviour
-}
+module.exports = ({ mode }) => ({
+  mode,
+  output: {
+    filename: 'bundle.js'
+  }
+});
+
+/**
+ * We can pass in env vars in to the config from the cli, hence, a function that returns an object
+ */
