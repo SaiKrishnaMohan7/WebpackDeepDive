9bf8777fed85daff91828ab2495e995c191f9371
Sai Krishna Mohan <saikrishnamohan@ibm.com>
2020-03-15 19:20:20 -0400

    source maps

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -1,6 +1,7 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = () => ({
+  devtool: 'source-map',
   output: {
     filename: 'bundle.js', // chunkhash is special string interpolation provided by webpack out of the box, cache busting can be used and clean webpack loader (plugin?) use
   },
